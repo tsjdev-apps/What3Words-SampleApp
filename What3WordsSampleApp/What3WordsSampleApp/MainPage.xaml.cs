c52f65472cc2ef726bd3dee2da9e7c1932842695
Sebastian Jensen <apps@tsjdev-apps.de>
2018-09-16 17:07:51 +0200

    - add basic styling and functionality

--- What3WordsSampleApp/What3WordsSampleApp/MainPage.xaml.cs
+++ What3WordsSampleApp/What3WordsSampleApp/MainPage.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using What3Words;
+using What3Words.Enums;
+using What3WordsSampleApp.Utils;
 using Xamarin.Forms;
 
 namespace What3WordsSampleApp
@@ -12,6 +12,40 @@ namespace What3WordsSampleApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            LatitudeLongitudeLanguagePicker.ItemsSource = EnumHelper.GetEnumAsEnumarable<LanguageCode>().ToList();
+            LatitudeLongitudeLanguagePicker.SelectedIndex = 2;
+
+            What3WordsLanguagePicker.ItemsSource = EnumHelper.GetEnumAsEnumarable<LanguageCode>().ToList();
+            What3WordsLanguagePicker.SelectedIndex = 2;
+        }
+
+        private async void GetWhat3WordsAddressButtonOnClicked(object sender, EventArgs e)
+        {
+            var languageCode = LatitudeLongitudeLanguagePicker.SelectedItem as LanguageCode?;
+            var what3wordsService = new What3WordsService(Statics.ApiKey, languageCode ?? LanguageCode.EN);
+
+            var latitude = double.Parse(LatitudeEntry.Text);
+            var longitude = double.Parse(LongitudeEntry.Text);
+            var result = await what3wordsService.GetReverseGeocodingAsync(latitude, longitude);
+
+            What3WordsAddressLabel.Text = result.Words;
+        }
+
+        private async void GetAddressFromWhat3WordsAddressButtonOnClicked(object sender, EventArgs e)
+        {
+            var languageCode = What3WordsLanguagePicker.SelectedItem as LanguageCode?;
+            var what3wordsService = new What3WordsService(Statics.ApiKey, languageCode ?? LanguageCode.EN);
+
+            var address = AddressEntry.Text;
+            var result = await what3wordsService.GetForewordGeocodingAsync(address);
+
+            AddressLabel.Text = $"Latitude: {result?.Geometry?.Lat} | Longitude: {result?.Geometry?.Lng}";
         }
     }
 }
